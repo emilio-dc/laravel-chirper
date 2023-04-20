387f48d65b0ce58ec44f4e76a5fc69b343a3cda9
Emilio Colombo <emiliocolombo3@gmail.com>
2023-04-20 10:05:04 -0300

    Chirp index response with Inertia

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return Inertia::render('Chirps/Index', [
+
+        ]);
     }
 
     /**
