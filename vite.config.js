17f17eebe3c8d23fb65d21df62aba7d3651f8437
Emilio <emiliocolombo3@gmail.com>
2023-04-27 15:05:14 -0300

    Fix for 0.0.0.0 issues
    When running VITE with the command
    sal npm run dev
    and then going to localhost in the browser, the app was not working
    because it was making requests to 0.0.0.0 and this was returning network
    errors.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
