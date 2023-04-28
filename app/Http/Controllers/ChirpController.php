977a35853260bf940af368d4b8ab72ef65f25400
Emilio <emiliocolombo3@gmail.com>
2023-04-28 09:19:36 -0300

    Small example for testing the controller

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return response('Hello, world! ' . date("Y-m-d h:i:sa"));
     }
 
     /**
