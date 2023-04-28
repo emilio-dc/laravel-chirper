4b954836d9a992adc3c612451315f83896003314
Emilio <emiliocolombo3@gmail.com>
2023-04-28 10:44:28 -0300

    Mass assignment protection for Chirp model

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     use HasFactory;
+
+    // Mass assignment protection: only allow for "message" propery (https://laravel.com/docs/eloquent#mass-assignment).
+    protected $fillable = [
+        'message'
+    ];
 }
