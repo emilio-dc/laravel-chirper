627863e96b65769eec7353d0e0b6fc4f009fdaa5
Emilio <emiliocolombo3@gmail.com>
2023-05-07 21:58:25 -0300

    Dispatch the ChirpCreated event
    The event is dispatched when a Chirp is inserted in the database using
    the create() method.

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,6 +14,10 @@ class Chirp extends Model
     // Mass assignment protection: only allow for "message" propery (https://laravel.com/docs/eloquent#mass-assignment).
     protected $fillable = [
         'message'
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class // Dispatch the ChirpCreated event when a new Chirp is created ( with the method create() which inserts it in the database ).
     ];
 
     public function user(): BelongsTo
